af92ab8613e453d5b3a89b0365224e68b315440a
CrushedPlate647 <ac.aarush.chouhdary@gmail.com>
2026-07-24 19:05:55 +0100

    yoo this is sick. added proper title, gave targets texture. first time making a commit.

--- target.gd
+++ target.gd
@@ -1,4 +1,11 @@
 extends Area3D
 
+func _ready() -> void:
+	var camera = get_viewport().get_camera_3d()
+	if camera:
+		look_at(camera.global_position, Vector3.UP)
+		rotate_y(deg_to_rad(180))
+
 func onHit() -> void:
 	queue_free()
+	
